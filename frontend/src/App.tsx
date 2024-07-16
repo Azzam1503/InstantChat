e2bb699aebf0bf1ad452c071baaaa5876ee9ae27
AZZAM UDDIN <uazzam69@gmail.com>
2024-07-17 01:40:49 +0530

    setted up components and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./pages/login/Login"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     hi, there
+      <div className="p-4 h-screen items-center justify-center">
+        <Login />
+      </div>
     </>
   )
 }
