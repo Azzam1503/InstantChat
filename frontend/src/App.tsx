f87efa189710d7d0cfb7a4b81e67ba8688cd4d43
AZZAM UDDIN <uazzam69@gmail.com>
2024-07-19 03:08:14 +0530

    setted up the react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import Login from "./pages/login/Login"
 import Signup from "./pages/signup/Signup"
 import Home from "./pages/home/Home"
+import { Route, Routes } from "react-router-dom"
 function App() {
 
 
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        {/* <Login /> */}
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
       </div>
     </>
   )
