ef27476ffff916be6aaa9f3e9f865fe3378c2ffb
AZZAM UDDIN <uazzam69@gmail.com>
2024-08-06 17:56:12 +0530

    deployment setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,33 @@
-import express, {Request, Response, Application} from "express";
+import express, {Request, Response} from "express";
+import path from "path";
 import dotevn from "dotenv";
 import authRoutes from "./routes/auth.routes";
 import dbConnect from "./service/dbConnect";
 import messageRoutes from "./routes/message.routes";
 import userRoutes from "./routes/user.routes";
 import cookieparser from "cookie-parser";
-import cors from "cors";
 import {app, server} from './socket/socket';
 
 dotevn.config();
 dbConnect();
 
 const PORT= process.env.PORT ||  3000;
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+
+const ___dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieparser());
-app.get("/", (req : Request, res: Response) =>{
-    res.send("hello");
-})
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+if(process.env.NODE_ENV !== "development"){
+    app.use(express.static(path.join(___dirname, "/frontend/dist")));
+    app.get("*", (req: Request, res: Response) => {
+        res.sendFile(path.join(___dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () =>{
     console.log(`Server is running on PORT ${PORT}`);
--- backend/src/service/dbConnect.ts
+++ backend/src/service/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_URI!);
+        await mongoose.connect("mongodb+srv://Azzam:azzam123@cluster0.kgnx0io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/chat-app");
         console.log("MongoDb connected successfully");
     } catch (error) {
         console.log(error)
